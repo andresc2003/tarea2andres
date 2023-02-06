1307ca138e061d7ed985fe452bbd2e993f167924
andresc2003 <cedenoandres18@gmail.com>
2023-02-06 15:29:06 -0600

    feat: se agrega el código de la tarea 2

---
+++ src/main/java/com/tarea2andres/tarea2andres/Tarea2andresApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2andres.tarea2andres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2andresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2andresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea2andres/tarea2andres/Tarea2andresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea2andres.tarea2andres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2andresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
